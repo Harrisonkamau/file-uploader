06d198837d9ffcc1931a825df8558f802f86b39c
Harrisonkamau <kamauharrison87@gmail.com>
2016-09-03 13:21:45 +0300

    create formdata and send AJAX request to the server

--- views/javascripts/upload.js
+++ views/javascripts/upload.js
@@ -3,3 +3,26 @@ $('.upload-btn').on('click', function (){
     $('.progress-bar').text('0%');
     $('.progress-bar').width('0%');
 });
+
+$('#upload-input').on('change', function(){
+
+  var files = $(this).get(0).files;
+
+  if (files.length > 0){
+    // One or more files selected, process the file upload
+
+    // create a FormData object which will be sent as the data payload in the
+    // AJAX request
+    var formData = new FormData();
+
+    // loop through all the selected files
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+
+      // add the files to formData object for the data payload
+      formData.append('uploads[]', file, file.name);
+    }
+
+  }
+
+});
