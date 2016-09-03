6bff7153242187e29cf361631739eb0297855918
Harrisonkamau <kamauharrison87@gmail.com>
2016-09-03 14:47:13 +0300

    remove files from views folder

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ var app = express();
 var path = require('path');
 var formidable = require('formidable');
 var fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
